0bbd0d7f351d319971d641382918c588c2ba10f9
Tori Rossini (RIT Student) <tar6248@ad.rit.edu>
2018-10-13 18:12:53 -0400

    Added Vuforia and UI

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] panels;
+    private GameObject currentPanel;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentPanel = panels[0];
+        for (int i = 0; i<panels.Length; i++)
+        {
+            if (i != 0)
+            {
+                panels[i].SetActive(false);
+            }
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+    public void SwitchScenes(int panelIndex)
+    {
+        currentPanel.SetActive(false);
+        panels[panelIndex].SetActive(true);
+        currentPanel = panels[panelIndex];
+        Debug.Log(currentPanel);
+        Debug.Log(currentPanel.activeInHierarchy);
+    }
+}
